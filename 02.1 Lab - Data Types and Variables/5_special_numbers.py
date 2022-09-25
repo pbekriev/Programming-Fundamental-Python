1f2209c07f684ff7776a424c66fdeeff87010c47
pbekriev <petar.bekriev@gmail.com>
2022-09-25 17:23:12 +0300

    Update 5_special_numbers.py

--- 02.1 Lab - Data Types and Variables/5_special_numbers.py
+++ 02.1 Lab - Data Types and Variables/5_special_numbers.py
@@ -1,12 +1,11 @@
-number = input()
+number = int(input())
+sum_of_digits = 0
 
-for num in range (1, int(number) + 1):
-    digit = str(num)
-
-    def sum_digits(digit):
-        return sum(int(x) for x in digit if x.isdigit())
-
-    if (sum_digits(digit) == 5) or (sum_digits(digit) == 7) or (sum_digits(digit) == 11):
-        print(f"{num} -> True")
+for loop_of_number in range(1, number + 1):
+    for digit in str(loop_of_number):
+        sum_of_digits += int(digit)
+    if sum_of_digits == 5 or sum_of_digits == 7 or sum_of_digits == 11:
+        print(f"{loop_of_number} -> True")
     else:
-        print(f"{num} -> False")
+        print(f"{loop_of_number} -> False")
+    sum_of_digits = 0
