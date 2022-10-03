7aa554923018d1c253a445f81cc9f9b90a0a8e33
pbekriev <petar.bekriev@gmail.com>
2022-10-03 22:07:52 +0300

    Update 9_hello_france.py
    still working

---
+++ 03.2 Exercise - Lists Basics/9_hello_france.py
@@ -0,0 +1,24 @@
+collection_of_item = input().split("|")
+budget = float(input())
+bought_item_price = []
+profit = 0
+for item_and_price in collection_of_item:
+    item_price = item_and_price.split("->")
+    item = item_price[0]
+    price = float(item_price[1])
+    if (item == "Clothes" and price <= 50.00) or \
+            (item == "Shoes" and price <= 35.00) or \
+            (item == "Accessories" and price <= 20.50):
+        if budget >= price:
+            budget -= round(price, 2)
+            profit += round((price * 0.4), 2)
+            bought_item_price.append(round((price * 1.4), 2))
+    if budget + sum(bought_item_price) >= 150:
+        break
+list_of_sales = [str(round(sale, 2)) for sale in bought_item_price]
+print(" ".join(list_of_sales))
+print(f"Profit: {profit:.2f}")
+if budget + sum(bought_item_price) >= 150:
+    print("Hello, France!")
+else:
+    print("Not enough money.")
