0d5b64fe55be36b46f5f3539c5a5f9c81269ece3
pbekriev <petar.bekriev@gmail.com>
2022-10-04 22:16:53 +0300

    Update 4_josephus_permutation.py

--- 03.3 More Exercise - List Basics/4_josephus_permutation.py
+++ 03.3 More Exercise - List Basics/4_josephus_permutation.py
@@ -1,11 +1,11 @@
 people_list = input().split()
 killed = int(input())
-killed_count = 0
+killed_index = 0
 killed_people = []
 while people_list:
-    killed_count += killed - 1
-    if killed_count >= len(people_list):
-        killed_count = killed_count % len(people_list)
-    killed_people.append(people_list[killed_count])
-    people_list.__delitem__(killed_count)
+    killed_index += killed - 1
+    if killed_index >= len(people_list):
+        killed_index = killed_index % len(people_list)
+    killed_people.append(people_list[killed_index])
+    people_list.__delitem__(killed_index)
 print([int(x) for x in killed_people])
