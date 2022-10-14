32eb30df33169d214bdd6cbdef0e5de42f80d41c
pbekriev <petar.bekriev@gmail.com>
2022-10-14 12:23:54 +0300

    Update 7_the_office.py

--- 05.1 Lab - List Advanced/7_the_office.py
+++ 05.1 Lab - List Advanced/7_the_office.py
@@ -1,9 +1,10 @@
 employees_happiness = list(map(int, input().split()))
 factor = int(input())
 multiply_by_factor = [x * factor for x in employees_happiness]
-avr_happyiness = sum(multiply_by_factor) / len(multiply_by_factor)
-happy_employees = [x for x in multiply_by_factor if x >= avr_happyiness]
-if len(happy_employees) >= len(multiply_by_factor) / 2:
-    print(f"Score: {len(happy_employees)}/{len(multiply_by_factor)}. Employees are happy!")
+happy_employeers = list(filter(lambda x: x >= sum(multiply_by_factor) / len(multiply_by_factor), multiply_by_factor))
+# avr_happyiness = sum(multiply_by_factor) / len(multiply_by_factor)
+# happy_employees = [x for x in multiply_by_factor if x >= avr_happyiness]
+if len(happy_employeers) >= len(multiply_by_factor) / 2:
+    print(f"Score: {len(happy_employeers)}/{len(multiply_by_factor)}. Employees are happy!")
 else:
-    print(f"Score: {len(happy_employees)}/{len(multiply_by_factor)}. Employees are not happy!")
+    print(f"Score: {len(happy_employeers)}/{len(multiply_by_factor)}. Employees are not happy!")
