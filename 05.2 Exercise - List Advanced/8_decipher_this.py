c214b52458fb9d9c7fbbba9318ecf8788fa6f5da
pbekriev <petar.bekriev@gmail.com>
2022-10-17 14:00:43 +0300

    Update 8_decipher_this.py

---
+++ 05.2 Exercise - List Advanced/8_decipher_this.py
@@ -0,0 +1,22 @@
+def decipher(word_for_decipher):
+    digits = ""
+    string = ""
+    for digit in word_for_decipher:
+        if digit.isdigit():
+            digits += digit
+        else:
+            string += digit
+    if len(string) > 1:
+        string_list = [x for x in string]
+        temp = string_list[0]
+        string_list[0] = string_list[len(string_list) - 1]
+        string_list[len(string_list) - 1] = temp
+        string = ''.join(string_list)
+    return chr(int(digits)) + string
+
+
+secret_message = input().split()
+decipher_message = []
+for secret_word in secret_message:
+    decipher_message.append(decipher(secret_word))
+print(" ".join(decipher_message))
