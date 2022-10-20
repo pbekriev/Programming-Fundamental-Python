9e498b2bf5392f52ae5388e1e0308fb27ab25eff
pbekriev <petar.bekriev@gmail.com>
2022-10-20 20:01:59 +0300

    Update 9_anonymous_threat.py
    not ready yet

--- 05.2 Exercise - List Advanced/9_anonymous_threat.py
+++ 05.2 Exercise - List Advanced/9_anonymous_threat.py
@@ -1,10 +1,30 @@
 def merge(list, start_index, end_index):
+    if start_index >= len(list):
+        start_index = len(list) - 1
+    elif start_index < -len(list):
+        start_index = -len(list)
+    if end_index >= len(list):
+        end_index = len(list) - 1
+    elif end_index < -len(list):
+        end_index = -len(list)
     list[start_index:end_index + 1] = ["".join(list[start_index:end_index + 1])]
     return list
 
 
 def divide(list, index, partitions):
-    pass
+    if index >= len(list):
+        index = len(list) - 1
+    elif index < -len(list):
+        index = -len(list)
+    split_item = list.pop(index)
+    split_list = []
+    divider = len(split_item) // partitions
+    for i in range(0, len(split_item), divider):
+        split_list.append(split_item[i:i + divider])
+    for x in split_list:
+        list.insert(index, x)
+        index += 1
+    return list
 
 
 some_string = input().split()
@@ -18,6 +38,6 @@ while command != "3:1":
     if action == "merge":
         merge(some_string, int(start_index_or_index), int(end_index_or_partitions))
     elif action == "divide":
-        pass
+        divide(some_string, int(start_index_or_index), int(end_index_or_partitions))
     command = input()
 print(" ".join(some_string))
