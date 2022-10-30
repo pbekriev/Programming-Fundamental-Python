67a9d5c0fb33649d4e7a9d0c4d12175f25b1af48
pbekriev <petar.bekriev@gmail.com>
2022-10-30 14:11:10 +0200

    Update 5_class.py

--- 06.2 Exercise - Classes and Objects/5_class.py
+++ 06.2 Exercise - Classes and Objects/5_class.py
@@ -6,20 +6,18 @@ class Class:
         self.students = []
         self.grades = []
 
-
     def add_student(self, name: str, grade: float):
-        if Class.__students_count > 0:
+        if Class.__students_count > len(self.students):
             self.students.append(name)
             self.grades.append(grade)
-            Class.__students_count -= 1
 
     def get_average_grade(self):
         average_grade = sum(self.grades) / len(self.grades)
         return f"{average_grade:.2f}"
 
     def __repr__(self):
-        average_grade = sum(self.grades) / len(self.grades)
-        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {average_grade:.2f}"
+        self.average_grade = sum(self.grades) / len(self.grades)
+        return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {Class.get_average_grade(self)}"
 
 
 a_class = Class("11B")
