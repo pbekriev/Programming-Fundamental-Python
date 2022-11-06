a46a7cf9c4a8fde51a8fee9b508dc73e045f847e
pbekriev <petar.bekriev@gmail.com>
2022-11-06 10:47:17 +0200

    Update 1_count_chars_in_a_string.py

--- 07.2 Exercise - Dictionaries/1_count_chars_in_a_string.py
+++ 07.2 Exercise - Dictionaries/1_count_chars_in_a_string.py
@@ -1,10 +1,10 @@
 some_string = input().split()
+one_word = "".join(some_string)
 my_dict = {}
 
-for item in some_string:
-    for char in item:
-        if char not in my_dict:
-            my_dict[char] = 0
-        my_dict[char] += 1
+for char in one_word:
+    if char not in my_dict:
+        my_dict[char] = 0
+    my_dict[char] += 1
 for key, value in my_dict.items():
     print(f"{key} -> {value}")
