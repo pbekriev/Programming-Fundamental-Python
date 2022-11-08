078b9b8c0fb64cb52538b79c923cc1d023228039
pbekriev <petar.bekriev@gmail.com>
2022-11-08 10:19:01 +0200

    Update 12_softuni_exam_results.py

--- 07.2 Exercise - Dictionaries/12_softuni_exam_results.py
+++ 07.2 Exercise - Dictionaries/12_softuni_exam_results.py
@@ -1,27 +1,3 @@
-# results = {}
-# submissions = {}
-# command = input()
-# while command != "exam finished":
-#     split_command = command.split("-")
-#     if "banned" in command:
-#         del results[split_command[0]]
-#     else:
-#         if split_command[0] not in results.keys() or results[split_command[0]] < split_command[2]:
-#             results[split_command[0]] = split_command[2]
-#
-#         if split_command[1] not in submissions.keys():
-#             submissions[split_command[1]] = 0
-#         submissions[split_command[1]] += 1
-#     command = input()
-#
-# print("Results:")
-# for username, point in results.items():
-#     print(f"{username} | {point}")
-# print("Submissions:")
-# for language, submissions_count in submissions.items():
-#     print(f"{language} - {submissions_count}")
-
-
 results = {}
 submissions = {}
 command = input()
@@ -30,21 +6,47 @@ while command != "exam finished":
     if "banned" in command:
         del results[split_command[0]]
     else:
-        if split_command[0] not in results.keys():
-            results[split_command[0]] = {}
-            results[split_command[0]][split_command[1]] = split_command[2]
-        if split_command[1] not in results[split_command[0]].keys() or results[split_command[0]][
-            split_command[1]] < split_command[2]:
-            results[split_command[0]][split_command[1]] = split_command[2]
+        username, language, points_str = split_command
+        points = int(points_str)
+        if username not in results.keys() or results[username] < points:
+            results[username] = points
 
-        if split_command[1] not in submissions.keys():
-            submissions[split_command[1]] = 0
-        submissions[split_command[1]] += 1
+        if language not in submissions.keys():
+            submissions[language] = 0
+        submissions[language] += 1
     command = input()
+
 print("Results:")
 for username, point in results.items():
-    for key, value in point.items():
-        print(f"{username} | {value}")
+    print(f"{username} | {point}")
 print("Submissions:")
 for language, submissions_count in submissions.items():
     print(f"{language} - {submissions_count}")
+
+
+# results = {}
+# submissions = {}
+# command = input()
+# while command != "exam finished":
+#     split_command = command.split("-")
+#     if "banned" in command:
+#         del results[split_command[0]]
+#     else:
+#         if split_command[0] not in results.keys():
+#             results[split_command[0]] = {}
+#             results[split_command[0]][split_command[1]] = split_command[2]
+#         if split_command[1] not in results[split_command[0]].keys() or results[split_command[0]][
+#             split_command[1]] < split_command[2]:
+#             results[split_command[0]][split_command[1]] = split_command[2]
+#
+#         if split_command[1] not in submissions.keys():
+#             submissions[split_command[1]] = 0
+#         submissions[split_command[1]] += 1
+#     command = input()
+# print("Results:")
+# for username, point in results.items():
+#     for key, value in point.items():
+#         print(f"{username} | {value}")
+# print("Submissions:")
+# for language, submissions_count in submissions.items():
+#     print(f"{language} - {submissions_count}")
