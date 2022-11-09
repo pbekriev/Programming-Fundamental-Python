16421c41ee80c53a35e3b288681252f25aee99ce
pbekriev <petar.bekriev@gmail.com>
2022-11-09 22:24:14 +0200

    Update 4_text_filter.py

--- 08.1 Lab - Text Processing/4_text_filter.py
+++ 08.1 Lab - Text Processing/4_text_filter.py
@@ -1,6 +1,5 @@
 ban_words = input().split(', ')
 text = input()
 for word in ban_words:
-    while word in text:
-        text = text.replace(word, '*' * len(word))
+    text = text.replace(word, '*' * len(word))
 print(text)
