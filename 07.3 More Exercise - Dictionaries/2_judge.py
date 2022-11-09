e35b7071eefea913d9f593411b157316cd3c4333
pbekriev <petar.bekriev@gmail.com>
2022-11-09 08:34:45 +0200

    Update 2_judge.py

--- 07.3 More Exercise - Dictionaries/2_judge.py
+++ 07.3 More Exercise - Dictionaries/2_judge.py
@@ -8,13 +8,20 @@ while command != "no more time":
         contests[contest] = {}
     if username not in contests[contest].keys() or contests[contest][username] < points:
         contests[contest][username] = points
-    if username not in individual.keys():
-        individual[username] = points
-    individual[username] += points
     command = input()
-
+for contest, user_points in contests.items():
+    for user, point in user_points.items():
+        if user not in individual.keys():
+            individual[user] = 0
+        individual[user] += point
 for key, value in contests.items():
     print(f"{key}: {len(value)} participants")
-    for key1, value1 in value.items():
-        num = 1
+    num = 1
+    for key1, value1 in sorted(value.items(), key=lambda x: (-x[1], x[0])):
         print(f"{num}. {key1} <::> {value1}")
+        num += 1
+print("Individual standings:")
+num = 1
+for name, total_points in sorted(individual.items(), key=lambda x: (-x[1], x[0])):
+    print(f"{num}. {name} -> {total_points}")
+    num += 1
