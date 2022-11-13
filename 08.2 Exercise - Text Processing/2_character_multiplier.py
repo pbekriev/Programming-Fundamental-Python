785171374bc0f214b65eae726a59245a2b520437
pbekriev <petar.bekriev@gmail.com>
2022-11-13 16:53:17 +0200

    Update 2_character_multiplier.py

--- 08.2 Exercise - Text Processing/2_character_multiplier.py
+++ 08.2 Exercise - Text Processing/2_character_multiplier.py
@@ -10,7 +10,7 @@ elif len(second_string) > len(first_string):
         total_sum += ord(first_string[index]) * ord(second_string[index])
     for index in range(len(first_string), len(second_string)):
         total_sum += ord(second_string[index])
-else: #len(first_string) == len(second_string)
+else:  # len(first_string) == len(second_string)
     for index in range(len(second_string)):
         total_sum += ord(first_string[index]) * ord(second_string[index])
 print(total_sum)
