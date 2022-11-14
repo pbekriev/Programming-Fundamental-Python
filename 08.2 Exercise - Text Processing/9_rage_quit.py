5dab8ea8d207e61e121e3cab49afa080487f5b49
pbekriev <petar.bekriev@gmail.com>
2022-11-14 09:45:22 +0200

    Update 9_rage_quit.py

---
+++ 08.2 Exercise - Text Processing/9_rage_quit.py
@@ -0,0 +1,12 @@
+some_string = input()
+unique_chart = ""
+output_string = ""
+last_index = 0
+for index in range(len(some_string)):
+    if some_string[index] not in unique_chart and not some_string[index].isdigit():
+        unique_chart += some_string[index].upper()
+    else:
+        output_string += some_string[last_index:index].upper() * int(some_string[index])
+        last_index = index + 1
+print(f"Unique symbols used: {len(unique_chart)}")
+print(output_string)
